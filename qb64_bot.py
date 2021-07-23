25e00d79eceefcd0a01cf6cbb8f5da48c73bde67
Ashish Kushwaha <ashishkushwahacb@gmail.com>
2021-07-23 12:54:52 +0530

    %qb64 now run code without  codeblocks

--- qb64_bot.py
+++ qb64_bot.py
@@ -1,5 +1,3 @@
-# @Author : AshishKingdom (Ashish Kushwaha)
-
 import os
 from dotenv import load_dotenv
 import discord
@@ -11,7 +9,6 @@ load_dotenv()
 
 client = discord.Client()
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-TEST_GUILD = os.getenv('DISCORD_GUILD_TESTING')
 
 
 #load all qb64/qbasic keywords
@@ -29,80 +26,95 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    contents = message.content.split('\n')
     # %qb64 command
-    if(contents[0].strip()=="%qb64"):
+    if(message.content[:5]=="%qb64"):
         response = ""
         execute = 0
         debug = False
-        if(len(contents)>=4):
-            
-            code_pos = [0, 0]
-            for i in range(0, len(contents)):
-                if (contents[i]=="```" or contents[i]=="```vb"):
-                    if(code_pos[0]==0):
-                        code_pos[0] = i+1 # starting line of code from message
-                    else:
-                        code_pos[1] = i
-                        break
-                #for debuging
-                if(contents[i]=="'$debug-bot-cloud"):
-                    debug = True
-            if(debug):response+="[QB64_BOT_DEBUG_MODE]\n"
 
-
-            if(code_pos[0]>0 and code_pos[1]>0 and code_pos[1]>code_pos[0]):
-            #if(contents[1]=="```" and contents[content_length-1]=="```"):
-                await message.add_reaction("⏳")
-
-                if(debug):response+="[QB64_BOT: attemtping to save the code in '/tmp/source.txt']\n"
-
-                try:
-                    f_hnd = open("/tmp/source.txt", 'w')
-                    f_hnd.write('\n'.join(contents[code_pos[0]:code_pos[1]]))
-                    f_hnd.close()
-                    if(debug):response+="[QB64-BOT: code saved in 'tmp/source.txt' successfully.]\n"
-                except OSError:
-                    response+="[QB64_BOT: Failed to write code in '/tmp/source.txt']\n"
-                    await message.add_reaction("❗")
-                    await message.clear_reaction("⏳")
-                    await message.reply("```\n{}\n```".format(response))
-                    return
-                #saving output of the program in out.txt using Luke's L-Basid
-
-                if(debug):response+="[QB64_BOT: running './bin/lbasic -t /tmp/source.txt>/tmp/out.txt']\n"
-
-                execute = os.system("./bin/lbasic -t /tmp/source.txt>/tmp/out.txt")
-
-                if(execute==1):
-                    if(debug):response+="[QB64_BOT: Some error occured. lbasic ended with 1.]\n"
-
-                if(debug): response+="[QB64_BOT: attemtping to read '/tmp/out.txt']\n"
-
-                try:
-                    f_hnd = open("/tmp/out.txt", "r")
-                    if(execute==5): #returns 5 if the program was stuck in some kind of infinite loop
-                        if(debug): response+="[QB64_BOT: Program execution exceeded more than 2 seconds]\n"
-                        response+= f_hnd.read(1500) + "\n" #in this case, we will send only 1024bytes of the output
-                    else:
-                        response+= f_hnd.read(1500) + "\n"
-                    f_hnd.close()
-                except OSError:
-                    response+="[QB64_BOT: Failed to read '/tmp/out.txt']\n"
-                    await message.add_reaction("❗")
-                    await message.clear_reaction("⏳")
-                    await message.reply("```\n{}\n```".format(response))
-                    return
-                #time_taken = time.time()-start
-                await message.add_reaction("✅")
-                await message.clear_reaction("⏳")
-                response = ">>> **Output :-**\n```\n"+response+"\n```"
-            else:
-                await message.add_reaction("❌") 
-                response = "> {} Please use proper codeblock syntax.".format(message.author.mention)
+        code_data = ""
+        vb_mentioned = False
+        # lets first look for ```vb in message (if user has used codeblocks)
+        code_block_pos = message.content.find("```vb")
+        if(code_block_pos == -1):
+            # ```vb not found.. so maybe user might have used simply ```
+            code_block_pos = message.content.find("```")
         else:
-            await message.add_reaction("❌")
-            response = "> {} You are using incorrect syntax.".format(message.author.mention)
+            # since vb was mentioned in ```vb, vb_mentioned will be set to true
+            vb_mentioned = True
+
+        if(code_block_pos == -1):
+            # ah.. so ``` is also not present in a message.
+            # now there are 2 possibilities, either user have just sent %qb64 which is wrong
+            # or user have sent the code without using codeblocks.
+            if(message.content[5:].strip()==''):
+                # only %qb64 was used by the user.
+                await message.add_reaction("❌")
+                await message.channel.send("> {} : No code found. Use %bot-help".format(message.author.mention))
+                return
+            else:
+                # the user has not used code-blocks. Maybe, the user was being lazy (:P)
+                # we will treat all the content after "%qb64 " as a code.
+                code_data = message.content[5:]
+        else:
+            #lets extract code from message
+            if(vb_mentioned):
+                p = message.content[5:].find("```vb")+5
+            else:
+                p = message.content[5:].find("```")+3
+
+            code_data = message.content[p,message.content.find("```", p)]
+
+        if(code_data.find("'$debug-bot-cloud")!=-1) : debug = True
+
+        if(debug) : response += "[QB64_BOT_DEBUG_MODE]\n"
+
+        await message.add_reaction("⏳")
+
+        if(debug):response+="[QB64_BOT: attemtping to save the code in '/tmp/source.txt']\n"
+
+        # saving the code data in '/tmp/source.txt'
+        try:
+            f_hnd = open("/tmp/source.txt", 'w')
+            f_hnd.write(code_data)
+            f_hnd.close()
+            if(debug):response+="[QB64-BOT: code saved in 'tmp/source.txt' successfully.]\n"
+        except OSError:
+            response+="[QB64_BOT: Failed to write code in '/tmp/source.txt']\n"
+            await message.add_reaction("❗")
+            await message.clear_reaction("⏳")
+            await message.reply("```\n{}\n```".format(response))
+            return
+
+        #saving output of the program in out.txt using Luke's L-Basid
+
+        if(debug):response+="[QB64_BOT: running './bin/lbasic -t /tmp/source.txt>/tmp/out.txt']\n"
+
+        execute = os.system("./bin/lbasic -t /tmp/source.txt>/tmp/out.txt")
+
+        if(execute==1):
+            if(debug):response+="[QB64_BOT: Some error occured. lbasic ended with 1.]\n"
+
+        if(debug): response+="[QB64_BOT: attemtping to read '/tmp/out.txt']\n"
+
+        try:
+            f_hnd = open("/tmp/out.txt", "r")
+            if(execute==5): #returns 5 if the program was stuck in some kind of infinite loop
+                if(debug): response+="[QB64_BOT: Program execution exceeded more than 2 seconds]\n"
+                response+= f_hnd.read(1500) + "\n" #in this case, we will send only 1024bytes of the output
+            else:
+                response+= f_hnd.read(1500) + "\n"
+            f_hnd.close()
+        except OSError:
+            response+="[QB64_BOT: Failed to read '/tmp/out.txt']\n"
+            await message.add_reaction("❗")
+            await message.clear_reaction("⏳")
+            await message.reply("```\n{}\n```".format(response))
+            return
+        #time_taken = time.time()-start
+        await message.add_reaction("✅")
+        await message.clear_reaction("⏳")
+        response = ">>> **Output :-**\n```\n"+response+"\n```"
         await message.channel.send(response)
         return
     else:
@@ -112,10 +124,10 @@ async def on_message(message):
             response = ""
             if(help_word in keywords_known):
                 await message.add_reaction("✅")
-                response = "> {} : http://qb64.org/wiki/".format(message.author.mention)+help_word
+                response = ">>> {} : http://qb64.org/wiki/".format(message.author.mention)+help_word
             else:
                 await message.add_reaction("❌")
-                response = "> {} : keyword not found. Try again. 🤷‍".format(message.author.mention)
+                response = ">>> {} : keyword not found. Try again. 🤷‍\n Use %bot-help to know about commands".format(message.author.mention)
             await message.channel.send(response)
             return
         # %help KEYWORD command
@@ -134,7 +146,7 @@ async def on_message(message):
                     response = "**{}** description exceed 2000 chars. 🤷‍ \n Use wiki - http://qb64.org/wiki/{}".format(doc_word, doc_word)
             else:
                 await message.add_reaction("❌")
-                response = "> {} keyword not found. Try again. 🤷‍".format(message.author.mention)
+                response = ">>> {} keyword not found. Try again. 🤷‍ \nUse %bot-help to know about commands".format(message.author.mention)
             
             await message.channel.send(response)
             return
@@ -149,7 +161,7 @@ async def on_message(message):
                     response = ">>> **{}** example code exceed 2000 chars. 🤷‍ \n Use wiki - http://qb64.org/wiki/{}".format(doc_word, doc_word)
             else:
                 await message.add_reaction("❌")
-                response = "> {} : keyword not found. Try again. :(".format(message.author.mention)
+                response = ">>> {} : keyword not found. Try again. :(\n Use %bot-help to know about commands".format(message.author.mention)
             
             await message.channel.send(response)
             return
@@ -157,7 +169,7 @@ async def on_message(message):
         if(message.content.strip()=="%bot-help"):
             response = ">>> **QB64 BOT Help -** \n\n"
             response += "_To run code using Luke's L-BASIC, use the following syntax :-_ \n%qb64\n\`\`\`\n'your code\n\`\`\`\n_OR_"
-            response += "\n%qb64\n\`\`\`vb\n'your code\n\`\`\`\n"
+            response += "\n%qb64\n\`\`\`vb\n'your code\n\`\`\`\nOR\n%qb64 your_code\n"
             response += "_To get full documentation for a keyword, use the following syntax :-_\n`%help keyword_name`\n"
             response += "_To get example for a keyword, use the following syntax :-_\n`%example keyword_name`\n"
             response += "_To get wiki link for a keyword, use the following syntax :-_\n`%wiki keyword_name`\n"
